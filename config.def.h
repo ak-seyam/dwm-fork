9ca48eb8631c56e7db17f982cb866f1e5f460578
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-06-08 15:23:31 +0200

    before nmaster display

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	{ "tm"           ,NULL  ,    NULL,       1 << 8,       0,           -1 },
 	{ "isis.exe"     ,NULL  ,    NULL,       1 << 4,       0,           -1 },
 	{ "Zathura"     ,NULL  ,    NULL,       1 << 3,       0,           -1 },
+	{ "Spotify"     ,NULL  ,    NULL,       1 << 6,       0,           -1 },
 };
 
 /* layout(s) */
@@ -73,13 +74,16 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_active, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *browsercmd[]  = { BRWOSER, NULL };
-static const char *filemanager[]  = { "alacritty", "-e", "ranger" ,NULL};
+static const char *filemanager[]  = { "kitty", "-e", "ranger" ,NULL};
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 static const char *screenshot[] = {SCREENSHOTTER, NULL};
+static const char *playpause[] = {"playerctl","play-pause", NULL};
+
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -90,10 +94,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ 0,                       XF86XK_MonBrightnessUp,      simple_exec,      {.v= "~/scripts/bctrl inc" } },
 	{ 0,                       XF86XK_MonBrightnessDown,      simple_exec,      {.v= "~/scripts/bctrl dec" } },
+	{ 0,                       XF86XK_AudioPlay,      simple_exec,      {.v= "playerctl play-pause" } },
+	{ 0,                       XF86XK_AudioPause,      simple_exec,      {.v= "playerctl play-pause"} },
+	{ 0,                       XF86XK_AudioNext,      simple_exec,      {.v= "playerctl next" } },
+	{ 0,                       XF86XK_AudioPrev,      simple_exec,      {.v= "playerctl previous" } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ ENABLE,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ DISABLE,                       XK_i,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ ENABLE,                       XK_z,      zoom,           {0} },
@@ -106,6 +112,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ DISABLE,             XK_t,      simple_exec,            {.v = "xinput --disable 10; xdotool key XF86AudioPlay" } },
+	{ ENABLE,             XK_t,      simple_exec,            {.v = "xinput --enable 10" } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
